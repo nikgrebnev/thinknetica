505d76370fa3d8222ab702c5a0b3e203466445f5
Nik Grebnev <nikolaygrebnev@gmail.com>
2019-09-12 15:42:38 +0300

    Lesson 1 - weight correction

--- 1/weight.rb
+++ 1/weight.rb
@@ -3,7 +3,7 @@
 puts "Please enter your name"
 name=gets.chomp
 puts "Please enter your height"
-height=gets.chomp.to_f
+height=Float(gets.chomp)
 height*=100 if height<2.5 #Если он ввел в метрах
 height*=30.48 if height<10.0 #Если он ввел в футах
 ideal=height-110.0
