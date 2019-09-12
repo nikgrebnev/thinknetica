bd06d80b052e120d42d3a4b66b04ed7b1684fa08
Nik Grebnev <nikolaygrebnev@gmail.com>
2019-09-12 14:37:36 +0300

    Lesson 1 triangle

---
+++ 1/triangle.rb
@@ -0,0 +1,12 @@
+puts "Please enter 3 sides of triangle separated by spaces"
+s=gets.split(" ").map { |i| i.to_f }.sort
+if s[0]==s[1] && s[1]==s[2]
+	puts "Это равносторонний треугольник"
+else
+	if s[0]==s[1] || s[1]==s[2]
+		puts "Это равнобедренный треугольник"
+	end
+	if s[0]**2 + s[1]**2 == s[2]**2
+		puts "Это прямоугольний треугольник"
+	end
+end
