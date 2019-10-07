5cbeda688747447be7d7897b1c7bc87c535cb2c0
Nik Grebnev <nikolaygrebnev@gmail.com>
2019-10-07 11:59:13 +0300

    lesson7, init

---
+++ 7/modules.rb
@@ -0,0 +1,51 @@
+module Producer
+  attr_accessor :producer
+
+=begin
+  def producer_set(producer)
+    self.producer = producer
+  end
+
+  def producer_get(producer)
+    self.producer
+  end
+=end
+end
+
+module InstanceCounter
+  def self.included(base)
+    base.extend ClassMethods
+    base.send :include, InstanceMethods
+  end
+
+  module ClassMethods
+    attr_writer :instances
+
+    def instances
+      @instances ||= 0
+    end
+  end
+
+  module InstanceMethods
+
+    def register_instance
+      self.class.instances += 1
+    end
+
+  end
+end
+
+module Functions
+  def self.included(base)
+    base.send :include, InstanceMethods
+  end
+
+  module InstanceMethods
+    def valid?
+      validate!
+      true
+    rescue
+      false
+    end
+  end
+end
---
+++ 7/station.rb
@@ -0,0 +1,48 @@
+class Station
+  include InstanceCounter, Functions
+
+  attr_reader :trains,:name
+
+  @@stations = []
+
+  NAME_FORMAT = /^[a-яa-z0-9\-, ]+$/i
+
+  def self.all
+    @@stations
+#    ObjectSpace.each_object(self).to_a
+  end
+
+  def initialize(name)
+    @name = name
+    validate!
+    @trains = []
+    self.register_instance
+    @@stations << self
+  end
+
+  def train_add(train)
+    @trains << train
+  end
+
+  def trains_print
+    trains_num = []
+    @trains.each { |train| trains_num << train.number }
+    trains_num
+  end
+
+  def trains_print_type(type)
+    trains_by_type = []
+    @trains.each { |train| trains_by_type << train.number if train.type == type }
+    trains_by_type
+  end
+  
+  def train_departure(train)
+    @trains.delete(train)
+  end 
+
+  protected
+
+  def validate!
+    raise "Некорректное название" if name !~ NAME_FORMAT
+  end
+end
